67f6fbdb74b25fe8eb5ffdea5a972393c33f516e
Jakub Bembenek <jakub.bembenekk@gmail.com>
2022-01-05 20:46:03 +0100

    Added left side panel with black hexes

--- Assets/Scripts/HexController.cs
+++ Assets/Scripts/HexController.cs
@@ -5,11 +5,9 @@ public class HexController : MonoBehaviour
 {
     public GameObject gameMeneger;
     public GameObject hexWrapper;
-    public TextMeshProUGUI text;
 
     private void OnMouseDown()
     {
-        text.text = hexWrapper.GetComponent<HexWrapperController>().piece.GetComponent<IPieceController>().GetPieceType().ToString();
         gameMeneger.GetComponent<GameManagerController>().HexSelected(hexWrapper);
     }
 }
